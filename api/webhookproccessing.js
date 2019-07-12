c84150c2829fb7f23370a67f63703431821d8c92
lanhhv84 <lanhhv84@gmail.com>
2019-07-12 08:42:18 +0700

    merge

--- api/webhookproccessing.js
+++ api/webhookproccessing.js
@@ -2,7 +2,7 @@ const { WebhookClient } = require('dialogflow-fulfillment');
 //const { Carousel } = require('actions-on-google');
 
 // processing by function
-const {makeReservation} = require('./makeflight');
+const {makeReservation, selectFlight} = require('./makeflight');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
@@ -22,7 +22,8 @@ module.exports = {
         
         intentMap.set('Default Welcome Intent', welcome);
         intentMap.set('Default Fallback Intent', fallback);
-        
+        intentMap.set('Make Reservation', makeReservation);
+        intentMap.set('Make Reservation - select.number', selectFlight)
         intentMap.set('', makeReservation);
 
         agent.handleRequest(intentMap);
